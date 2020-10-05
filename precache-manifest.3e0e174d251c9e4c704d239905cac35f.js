52cf1263ebf5181dce19e66b5d0a395f1e55013a
Marcin <mzmuda627@gmail.com>
2020-10-05 20:20:01 +0200

    Updates

---
+++ precache-manifest.3e0e174d251c9e4c704d239905cac35f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "60e304f273f7f8e92179",
+    "url": "/Country-quiz/static/css/main.541fafc8.chunk.css"
+  },
+  {
+    "revision": "60e304f273f7f8e92179",
+    "url": "/Country-quiz/static/js/main.3bd09620.chunk.js"
+  },
+  {
+    "revision": "c7b6b81eb71f235e7935",
+    "url": "/Country-quiz/static/js/runtime~main.9d6aee9e.js"
+  },
+  {
+    "revision": "f05c2a2869ce66bc7fd6",
+    "url": "/Country-quiz/static/js/2.511dca02.chunk.js"
+  },
+  {
+    "revision": "82d9605afcc8a7a321691203895e1aa3",
+    "url": "/Country-quiz/static/media/win.82d9605a.svg"
+  },
+  {
+    "revision": "7e2f750c3fa6f8e7240e017f2fae0113",
+    "url": "/Country-quiz/static/media/background.7e2f750c.png"
+  },
+  {
+    "revision": "b3c9330ae2ac1bac93dcee5c48c0ef74",
+    "url": "/Country-quiz/index.html"
+  }
+];
